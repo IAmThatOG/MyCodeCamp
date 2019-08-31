6a7f2efda4641d2c73b2e58e04e5f5be698cb2e9
IAmThatOG <gabbygabbylexy@gmail.com>
2019-08-31 08:55:54 +0100

    created entity classes

--- MyCodeCamp.Data/Entities/Camp.cs
+++ MyCodeCamp.Data/Entities/Camp.cs
@@ -1,7 +1,22 @@
+using System.Collections.Generic;
+using System;
 namespace MyCodeCamp.Data.Entities
 {
     public class Camp
     {
-        
+        public int Id { get; set; }
+        public string Moniker { get; set; }
+        public string Name { get; set; }
+        public DateTime EventDate { get; set; } = DateTime.MinValue();
+        public int Length { get; set; }
+        public string Description { get; set; }
+        public Location Location { get; set; }
+        public ICollection<Speaker> Speakers { get; set; }
+
+        public Camp()
+        {
+            Speakers = new ICollection<Speaker>();
+        }
+
     }
 }
